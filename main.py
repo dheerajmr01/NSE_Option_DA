d061f727d17fdd8f2e5f54d79d782c4bb50ef7b9
dheerajmr01 <dheerajmr01@gmail.com>
2023-02-02 17:18:12 +0530

    evening

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from time import sleep
+import os
+
 
 
 website = "https://www.nseindia.com/option-chain"
 path = "C:/Users/dheer/OneDrive/Desktop/Options/chromedriver.exe"
 chromeOptions = webdriver.ChromeOptions()
-service = Service(executable_path=path)
-driver = webdriver.Chrome(service=service, chrome_options=chromeOptions)
 
+p = {"download.default_directory" : "D:\\Projects\\NSE_Option_DA"}
+chromeOptions.add_experimental_option("prefs",p)
+service = Service(executable_path=path)
+driver = webdriver.Chrome(service=service, options=chromeOptions)
 
 driver.get(website)
 sleep(3)
 driver.find_element(by="xpath", value='//div[@class="xlsdownload"]').click()
 sleep(7)
+driver.close()
+
+sleep(10)
+
+os.renames("option-chain-ED-NIFTY-02-Feb-2023", "d1")
+
